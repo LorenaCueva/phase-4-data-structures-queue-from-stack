f70c9dac12ed4c0248a07a1c72e021ba3b67ecef
Lorena Cueva <lorenacueva@gmail.com>
2023-01-23 11:20:04 -0800

    commit tree 31790a89691867bb85ef6af11e3dd11098850120

--- lib/queue_from_stack.rb
+++ lib/queue_from_stack.rb
@@ -1,3 +1,27 @@
 require_relative './stack'
 
-# your code here
+class MyQueue
+    
+    def initialize
+        @s1 = Stack.new
+        @s2 = Stack.new
+    end
+
+    def add(value)
+        move(@s2, @s1)
+        @s1.push(value)
+        move(@s1, @s2)
+    end
+
+    def remove
+        @s2.pop
+    end
+
+    def peek
+        @s2.peek
+    end
+
+    def move(here, there)
+        there.push(here.pop) until here.empty?
+    end
+end
